a386e7e42481ebabea07a0175906f8892c218eb4
Zain Rahman <91568498+ZainRahman23@users.noreply.github.com>
2025-08-30 20:08:36 -0500

    q2 done

---
+++ Assets/Scripts/Kill Zone.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Q2 {
+    public class KillZone : MonoBehaviour
+    {
+        void OnTriggerEnter(Collider other) {
+            if (other.GetComponent<CharacterController>()) {
+                string currentScene = SceneManager.GetActiveScene().name;
+                SceneManager.LoadScene(currentScene);
+            }
+        }
+    }
+}
+
