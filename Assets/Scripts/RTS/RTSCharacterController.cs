5a44caa9258762b7aebe43e062fe3b19f1786b1a
Zain Rahman <91568498+ZainRahman23@users.noreply.github.com>
2025-12-01 10:00:00 -0600

    Q10 Done

--- Assets/Scripts/RTS/RTSCharacterController.cs
+++ Assets/Scripts/RTS/RTSCharacterController.cs
@@ -1,25 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
-namespace RTS {
-    public class RTSCharacterController : MonoBehaviour
-    {
-        // Outlets
-        Animator animator;
-        NavMeshAgent navAgent;
+namespace RTS
+{
+    public class RTSCharacterController : MonoBehaviour
+    {
+        // Outlets
+        Animator animator;
+        UnityEngine.AI.NavMeshAgent navAgent;
+        public GameObject selectionIndicator;
+        public GameObject attackTarget;
+        public GameObject projectilePrefab;
+        
+        // Configuration
+        public float attackDistance;
+        public float attackDelay;
+        
+        // State Tracking
+        float _timeSinceLastAttack;
+        
+        // Methods
+        public void Select()
+        {
+            selectionIndicator.SetActive(true);
+        }
 
-        // Methods
-        void Start() {
-            animator = GetComponent<Animator>();
-            navAgent = GetComponent<NavMeshAgent>();
-        }
+        public void Deselect()
+        {
+            selectionIndicator.SetActive(false);
+        }
 
-        void Update() {
-            animator.SetFloat("velocity", navAgent.velocity.magnitude);
-        }
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+            navAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        }
 
-        public void SetDestination(Vector3 targetPosition) {
-            navAgent.destination = targetPosition;
-        }
-    }
+        void Update()
+        {
+            animator.SetFloat("velocity", navAgent.velocity.magnitude);
+            
+            // Increment attack timer
+            _timeSinceLastAttack += Time.deltaTime;
+            
+            // Prepare to get in range of target
+            if (attackTarget)
+            {
+                // Prepare to attack
+                SetDestination(attackTarget.transform.position);
+                if (Vector3.Distance(attackTarget.transform.position, transform.position) <= attackDistance)
+                {
+                    // Perform Attack
+                    if (_timeSinceLastAttack >= attackDelay)
+                    {
+                        // Reset attack timer
+                        _timeSinceLastAttack = 0;
+                        
+                        // Prepare attack
+                        // For prototype visibility: Attack from slightly above the player
+                        Vector3 projectileOrigin = transform.position + new Vector3(0, 2f, 0);
+                        Vector3 directionToTarget = (attackTarget.transform.position - projectileOrigin).normalized;
+                        
+                        // Shoot projectile
+                        GameObject projectile = Instantiate(
+                            projectilePrefab,
+                            projectileOrigin,
+                            Quaternion.LookRotation(directionToTarget)
+                        );
+                        
+                        // For prototype visibility: Make the projectile large
+                        projectile.transform.localScale = Vector3.one * 10f;
+                        projectile.GetComponent<Rigidbody>().AddForce(directionToTarget * 50f, ForceMode.Impulse);
+                        
+                        // Cleanup projectile
+                        Destroy(projectile, 10f);
+                    }
+                }
+            }
+        }
+
+        public void SetDestination(Vector3 targetPosition)
+        {
+            navAgent.destination = targetPosition;
+        }
+        
+        public void SetTarget(GameObject target)
+        {
+            attackTarget = target;
+        }
+    }
 }
+
+
--- Assets/Scripts/RTS/RTSGameController.cs
+++ Assets/Scripts/RTS/RTSGameController.cs
@@ -5,58 +5,159 @@ using UnityEngine.InputSystem;
 
 namespace RTS
 {
-    public class RTSGameController : MonoBehaviour
-    {
-        // State Tracking
-        public GameObject currentSelection;
-        
-        // Methods
-        void Update()
-        {
-            // Mouse Interactions
-            Mouse mouse = Mouse.current;
-            if (mouse != null)
-            {
-                // Selection Command
-                if (mouse.leftButton.wasPressedThisFrame)
-                {
-                    // Get all objects under mouse cursor
-                    Ray selectionRaycast = Camera.main.ScreenPointToRay(mouse.position.ReadValue());
-                    RaycastHit[] hits = Physics.RaycastAll(selectionRaycast);
-                    
-                    // See if any are selectable
-                    currentSelection = null;
-                    foreach (RaycastHit hit in hits)
-                    {
-                        if (hit.collider.GetComponent<RTSCharacterController>())
-                        {
-                            currentSelection = hit.collider.gameObject;
-                        }
-                    }
-                }
-                
-                // Interaction Command
-                if (mouse.rightButton.wasPressedThisFrame && currentSelection)
-                {
-                    RTSCharacterController character = currentSelection.GetComponent<RTSCharacterController>();
-                    if (character)
-                    {
-                        // Get all objects under the mouse cursor
-                        Ray selectionRaycast = Camera.main.ScreenPointToRay(mouse.position.ReadValue());
-                        RaycastHit[] hits = Physics.RaycastAll(selectionRaycast);
-                        
-                        // Check for possible interactions
-                        foreach (RaycastHit hit in hits)
-                        {
-                            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
-                            {
-                                // Move player to destination
-                                character.SetDestination(hit.point);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+    public class RTSGameController : MonoBehaviour
+    {
+        // Outlets
+        public Canvas uiCanvas;
+        public RectTransform selectionBox;
+
+        // Configuration
+        float mouseDragThreshold = 3f;
+
+        // State Tracking
+        public List<GameObject> currentSelection;
+        public Vector2 mouseClickStart;
+        
+        // Methods
+        void Start(){
+            currentSelection = new List<GameObject>();
+        }
+
+        void Update()
+        {
+            // Mouse Interactions
+            Mouse mouse = Mouse.current;
+            if (mouse != null)
+            {
+                // Start Drag
+                if(mouse.leftButton.wasPressedThisFrame){
+                    mouseClickStart = mouse.position.ReadValue();
+                }
+
+                // Handle Drag
+                if(mouse.leftButton.isPressed){
+                    Vector2 mousePosition = mouse.position.ReadValue();
+                    if(Vector2.Distance(mouseClickStart, mousePosition) > mouseDragThreshold){
+                        selectionBox.gameObject.SetActive(true);
+
+                        // Compute Midpoint and Assign Position
+                        Vector2 boxMidpoint = Vector2.Lerp(mouseClickStart, mousePosition, 0.5f);
+                        selectionBox.anchoredPosition = boxMidpoint / uiCanvas.scaleFactor;
+
+                        // Compute Drag Bounds and Assign Size
+                        Vector2 box = new Vector2(
+                            Mathf.Abs(mouseClickStart.x - mousePosition.x),
+                            Mathf.Abs(mouseClickStart.y - mousePosition.y)
+                        );
+                        selectionBox.sizeDelta = box / uiCanvas.scaleFactor;
+                    }
+                }
+
+                // Selection Command
+                if (mouse.leftButton.wasReleasedThisFrame)
+                {
+                    if (selectionBox.gameObject.activeInHierarchy)
+                    {
+                        // Mouse Drag = Get all eligible objects
+                        SelectWithinBox();
+                    }
+                    else
+                    {
+                        // Single Click
+                        SelectUnderMouse();
+                    }
+
+                    // Hide selection box
+                    selectionBox.gameObject.SetActive(false);
+                }
+                
+                // Interaction Command
+                if (mouse.rightButton.wasPressedThisFrame && currentSelection.Count > 0)
+                {
+                    foreach(GameObject selection in currentSelection){ 
+                        RTSCharacterController character = selection.GetComponent<RTSCharacterController>();
+                        if (character)
+                        {
+                            // Get all objects under the mouse cursor
+                            Ray selectionRaycast = Camera.main.ScreenPointToRay(mouse.position.ReadValue());
+                            RaycastHit[] hits = Physics.RaycastAll(selectionRaycast);
+                        
+                            // Check for possible interactions
+                            foreach (RaycastHit hit in hits)
+                            {
+                                if (hit.collider.gameObject.layer != LayerMask.NameToLayer("Ground"))
+                                {
+                                    character.SetTarget(hit.collider.gameObject);
+                                    break;
+                                }
+
+                                if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+                                {
+                                    character.SetTarget(null);
+                                    character.SetDestination(hit.point);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        void SelectUnderMouse(){
+            // Get all objects under mouse cursor
+            Ray selectionRaycast = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+            RaycastHit[] hits = Physics.RaycastAll(selectionRaycast);
+
+            // See if any are selectable
+            DeselectAll();
+            foreach(RaycastHit hit in hits){
+                if(hit.collider.GetComponent<RTSCharacterController>()){
+                    currentSelection.Add(hit.collider.gameObject);
+                    hit.collider.SendMessage("Select", SendMessageOptions.DontRequireReceiver);
+                }
+            }
+        }
+
+        void SelectWithinBox()
+        {
+            RTSCharacterController[] characterControllers = FindObjectsOfType<RTSCharacterController>();
+            
+            // Loop through each eligible object to see if it's within our selection box boundaries
+            DeselectAll();
+            foreach (RTSCharacterController character in characterControllers)
+            {
+                // Translate its 3D position to 2D screen space
+                Vector2 characterPosition = Camera.main.WorldToScreenPoint(character.transform.position);
+                
+                // Adjust screen space for any UI scaling
+                characterPosition = characterPosition / uiCanvas.scaleFactor;
+                
+                // Build a rectangle that represents where our selection box is on screen
+                Rect anchoredRect = new Rect(
+                    selectionBox.anchoredPosition.x - selectionBox.sizeDelta.x / 2f,
+                    selectionBox.anchoredPosition.y - selectionBox.sizeDelta.y / 2f,
+                    selectionBox.sizeDelta.x,
+                    selectionBox.sizeDelta.y
+                );
+                
+                // Does the character fall inside the box
+                if (anchoredRect.Contains(characterPosition))
+                {
+                    currentSelection.Add(character.gameObject);
+                    character.SendMessage("Select", SendMessageOptions.DontRequireReceiver);
+                }
+            }
+        }
+
+        void DeselectAll()
+        {
+            foreach (GameObject selection in currentSelection)
+            {
+                selection.SendMessage("Deselect", SendMessageOptions.DontRequireReceiver);
+            }
+
+            currentSelection.Clear();
+        }
+    }
 }
+
